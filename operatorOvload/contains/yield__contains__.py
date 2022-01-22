e19cb39fe86a3855c5b6887fcc512fead7186ee6
Eliot <halychprogramming@gmail.com>
2022-01-22 20:35:15 +0200

    Forget to add a doc string in __contains__

--- operatorOvload/contains/yield__contains__.py
+++ operatorOvload/contains/yield__contains__.py
@@ -1,8 +1,7 @@
 class Iters:
-
     """
-    This class do the same as 'Iters' class from __contains__iter__next.py
-    But it is much shorter and also more readable than the code from the file mentioned above
+    This class does the same as 'Iters' class from __contains__iter__next.py,
+    but it is much shorter and also more readable than the code from the file mentioned above
     """
 
     def __init__(self, value):
@@ -16,4 +15,7 @@ class Iters:
             yield item
 
     def __contains__(self, value):
+        """
+        This method is used to check if param value is contained in self.data(it can be any other sequence)
+        """
         return value in self.data
