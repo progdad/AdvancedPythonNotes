1f2ef6c66c1173588699e702de5c7adf464c2922
Eliot <halychprogramming@gmail.com>
2022-01-25 00:45:47 +0200

    Main example of __setattr__

--- operatorOvload/__getattr__setattr__/__setattr__base.py
+++ operatorOvload/__getattr__setattr__/__setattr__base.py
@@ -1,10 +1,26 @@
-class Access:
-    age = 30
+class Avanger:
+    def __init__(self, age=27):
+        self.age = age
 
     def __setattr__(self, key, value):
-        if key == "age":
+
+        """
+        __setattr__ always takes over the role of adding new variables in class.
+        You have to use <self.__dict__[key]> to add new variable.
+
+        :param key: in <acc.age = 10> age is key
+        :param value: in <acc.age = 10> 10 is value
+        """
+
+        if key in ["age", "name", "surname", "gender", "resp"]:
             self.__dict__[key] = value
+        else:
+            raise PermissionError(f"no permissions to add '{key}' attribute")
 
 
-acc = Access()
-acc.age = 100
+blackwidow = Avanger()
+blackwidow.name, blackwidow.surname = "Natasha", "Romanoff"
+blackwidow.gender = "Woman"
+print(blackwidow.__dict__)
+
+blackwidow.status = "Dead :("
