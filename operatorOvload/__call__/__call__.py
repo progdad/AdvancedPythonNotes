ea5de3127c6fb1fbaf528f032715eafb6d4d2d29
Eliot <halychprogramming@gmail.com>
2022-01-27 19:45:19 +0200

    __call__ magic method

---
+++ operatorOvload/__call__/__call__.py
@@ -0,0 +1,11 @@
+class ForAPI:
+    def __call__(self, *args, **kwargs):
+        """
+            Is used to handle any arguments called by class instances.
+            Third by popularity magic method after __init__ constructor and __repr__, __str__
+        """
+        return 'Called: ', args, kwargs
+
+
+callableCls = ForAPI()
+print(callableCls('Arguments', 1, 2, 3, ddd=222, soup="Beautifulsoup"))  # ('Called: ', ('Arguments', 1, 2, 3), {'ddd': 222, 'soup': 'Beautifulsoup'})
