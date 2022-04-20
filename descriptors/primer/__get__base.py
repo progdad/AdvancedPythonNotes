d89e784f84bc126104e885b69252343ea4341aa0
Eliot <halychprogramming@gmail.com>
2022-04-20 18:07:48 +0300

    Two base examples of __get__ method and how descriptors work

---
+++ descriptors/primer/__get__base.py
@@ -0,0 +1,30 @@
+# All the examples of descriptors are taken from the official python documentation https://docs.python.org/3/howto/descriptor.html
+
+
+class Descriptor:
+    """
+        Descriptor is an any object which defines the methods __get__(), __set__(), or __delete__().
+        When a class attribute is a descriptor, its special binding behavior is triggered upon attribute lookup.
+        Normally, using a.b to get, set or delete an attribute looks up the object named b in the class dictionary for a,
+        but if b is a descriptor, the respective descriptor method gets called.
+        TO USE THE DESCRIPTOR, IT MUST BE STORED AS A CLASS VARIABLE IN ANOTHER CLASS
+    """
+
+    def __get__(self, obj, objtype=None):
+        return 10
+
+
+class A:
+    x = 5
+    y = Descriptor()
+
+
+d = Descriptor()
+a = A()
+
+print(d)  # <__main__.Ten object at 0x7fd949f79fa0>
+print(a.y)  # 10
+
+# In the a.x attribute lookup, the dot operator finds 'x': 5 in the class dictionary.
+# In the a.y lookup, the dot operator finds a descriptor instance, recognized by its __get__ method.
+# Calling that method returns 10.
---
+++ descriptors/primer/__get__base2.py
@@ -0,0 +1,30 @@
+import os
+
+
+class DirectorySize:
+    def __get__(self, obj, objtype=None):
+        print(obj, '_____', objtype)  # Instance of directory, self.dirname="../../" _____ <class '__main__.Directory'>
+        return len(os.listdir(obj.dirname))
+
+
+class Directory:
+    size = DirectorySize()
+
+    def __init__(self, dirname):
+        self.dirname = dirname
+
+    def __repr__(self):
+        return f"Instance of directory, self.dirname=\"{self.dirname}\""
+
+
+dir1 = Directory("../../")
+dir2 = Directory("../../operatorOvload")
+print(dir1.size)  # 5
+print(dir2.size)  # 10
+
+# Besides showing how descriptors can run computations,
+# this example also reveals the purpose of the parameters to __get__().
+# The "self" parameter is "size", an instance of "DirectorySize" class.
+# The "obj" parameter is either dir1 or dir2, an instance of Directory.
+# It is the obj parameter that lets the __get__() method learn the target directory.
+# The objtype parameter is the class Directory.
